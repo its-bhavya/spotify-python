3b054bf50bd3aea5508fdc4efb17e2f87ee96c3a
its-bhavya <bhavyaa716@gmail.com>
2025-02-15 11:39:36 +0530

    define the scope and params

--- main.py
+++ main.py
@@ -17,6 +17,19 @@ AUTH_URL='https://accounts.spotify.com/authorize'
 TOKEN_URL='https://accounts.spotify.com/api/token'
 API_BASE_URL='https://api.spotify.com/v1/'
 
+@app.route('/')
+def index():
+    return "Welcome! <a href = '/login'>Login with Spotify</a>"
 
+@app.route('/login')
+def login():
+    scope = 'playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-read-email user-read-private' 
 
+    params = {
+        'client_id' : CLIENT_ID,
+        'response_type' : 'code',
+        'scope' : scope,
+        'redirect_uri' : REDIRECT_URI,
+        'show_dialog' : True
+    }
 
